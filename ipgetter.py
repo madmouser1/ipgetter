043ecb5a9801e6946cf2730c4ae579e14f8ffcfe
phoemur <phoemur@users.noreply.github.com>
2014-03-10 22:21:09 -0300

    Update ipgetter.py

--- ipgetter.py
+++ ipgetter.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
+"""
+This module is designed to fetch your external IP address from the internet.
+It is used mostly when behind a NAT.
+It picks your IP randomly from a serverlist to minimize request overhead on a single server
+
+If you want to add or remove your server from the list contact me on github
+
+
+API Usage
+=========
+
+    >>> import ipgetter
+    >>> myip = ipgetter.myip()
+    >>> myip
+    '8.8.8.8'
+"""
 
 import re
 import random
